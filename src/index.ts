3559b25193a7cb72e5e64b8836cf6bedc6705a41
George Zhang <275132122@qq.com>
2017-12-10 22:51:39 +0800

    设置允许方法头

--- src/index.ts
+++ src/index.ts
@@ -40,6 +40,7 @@ app.use(configuredPassport.session());
 app.all('*', (req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     next();
 });
 
